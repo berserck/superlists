0cc5a9cbccba3cd1c337d59523c1c0d88a5c0049
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-24 18:45:33 +0200

    Now test checks if we are really using the template file

--- lists/tests.py
+++ lists/tests.py
@@ -2,16 +2,13 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-
+from django.template.loader import render_to_string
 
 class HomePAgeViewTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_home_template(self):
         request = HttpRequest()
-
         response = home_page(request)
 
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        print(response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
