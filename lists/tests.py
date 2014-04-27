fe33bd3b93d1b88bc44a2a550b8965320d45846d
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-27 14:06:31 +0200

    Added post method

--- lists/tests.py
+++ lists/tests.py
@@ -11,4 +11,14 @@ class HomePAgeViewTest(TestCase):
         response = home_page(request)
 
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.content.decode('utf8'), expected_html)
+
+    def test_home_page_can_handle_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'a new item'
+        response = home_page(request)
+        self.assertIn('a new item', response.content.decode())
+
+        expected_html = render_to_string('home.html',{'new_item_text':'a new item'})
+        self.assertEqual(response.content.decode('utf8'),expected_html)
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        new_item = request.POST['item_text']
+    else:
+        new_item = ''
+
+    return render(request, 'home.html', {'new_item_text': new_item})
