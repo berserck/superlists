4cb863809ba917daa0b70a3c983a3cadcbc5c726
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-27 14:06:48 +0200

    Added test to verify second item

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,6 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-
     def test_starting_a_todo_list(self):
         # Edith has heard about a cool new online to-do app. she goes
         # to check out its home page
@@ -36,12 +35,19 @@ class NewVisitorTest(unittest.TestCase):
         # 1: Buy peacock feathers as an item in a to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows] )
-        # There is still a text box inviting her to add another item. She
+        #There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The pages update again, and now both items are on the list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 
 
