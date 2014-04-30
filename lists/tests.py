28ed1cbdbe695c6339fc7a93da00c419639d32b1
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-30 11:46:44 +0200

    Added HomePageTest

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,17 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 from django.template.loader import render_to_string
+from django.core.urlresolvers import resolve
 
-class HomePAgeViewTest(TestCase):
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+class HomePageViewTest(TestCase):
 
     def test_home_page_uses_home_template(self):
         request = HttpRequest()
@@ -17,8 +26,11 @@ class HomePAgeViewTest(TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'a new item'
+
         response = home_page(request)
+
         self.assertIn('a new item', response.content.decode())
 
         expected_html = render_to_string('home.html',{'new_item_text':'a new item'})
+
         self.assertEqual(response.content.decode('utf8'),expected_html)
