2556a33b60f1c431c688cb83d1b5b027b4ffccec
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-30 17:52:34 +0200

    shufling things to match book order

--- lists/tests.py
+++ lists/tests.py
@@ -20,9 +20,6 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-
-class HomePageViewTest(TestCase):
-
     def test_home_page_uses_home_template(self):
         request = HttpRequest()
         response = home_page(request)
@@ -30,21 +27,47 @@ class HomePageViewTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode('utf8'), expected_html)
 
-    def test_home_page_can_save_post_request(self):
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
-        request.POST['item_text'] = 'a new item'
+        request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'a new item')
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
+
+        
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -65,16 +88,5 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 
-class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-        self.assertContains(response, 'item 1')
-        self.assertContains(response, 'item 2')
