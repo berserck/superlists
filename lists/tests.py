244d6393d717d91984bc7025504342a278c0afde
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-30 18:09:20 +0200

    added creating new list via post, using django testing facilities

--- lists/tests.py
+++ lists/tests.py
@@ -15,11 +15,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
     def test_home_page_uses_home_template(self):
         request = HttpRequest()
         response = home_page(request)
@@ -27,29 +22,20 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode('utf8'), expected_html)
 
+
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
 
     def test_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ListViewTest(TestCase):
@@ -67,7 +53,7 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
 
-        
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
