ca7a4dc0f2cb2e0aca735b0a259470c0829ef6d4
Pedro Parracho <pedro.parracho@gmail.com>
2014-04-30 18:13:54 +0200

    added creating new list via post, using django testing facilities

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -54,16 +54,24 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'item 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -71,7 +79,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 
