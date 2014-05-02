be8375d30db475ea41c6eb1477a2a8a3728599c7
Pedro Parracho <pedro.parracho@gmail.com>
2014-05-02 14:44:33 +0200

    Moved utrls to inside teh lists

--- lists/views.py
+++ lists/views.py
@@ -18,6 +18,7 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
 
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
